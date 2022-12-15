222a8065601598217eafb434d9ac0bcaaebac6a7
Anniessa Antar <anniessa.antar@gmail.com>
2022-12-15 15:22:06 -0600

    answered question 4

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -15,11 +15,12 @@ console.log(supplyChanges);
 
 // 3. Console log the value of the second item in the 'supplyChanges' array
 console.log('3. Second supplyChange is:');
-
+console.log(supplyChanges[1]);
 
 // 4. The last item was added by mistake. Remove it from the 'supplyChanges' 
 //    array & console.log the value removed.
 console.log('4. Removed item:');
+console.log (supplyChanges.pop());
 
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 console.log('5. Adding 25 to supplyChanges.');
